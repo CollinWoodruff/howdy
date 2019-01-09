8f7c4c53f9bd02c6ed884d4bebc53df3aa67ed7e
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-09 13:16:43 -0500

    A basic index.php file

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Collin Woodruff
+ * Date: 1/7/2019
+ * Time: 11:20 AM
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Howdy!</h1>';
+
+    $view = new View;
+    gecho $view->render('views/home-page.html');
+});
+
+//Run fat free
+$f3->run();
